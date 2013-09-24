d23c1492d89bb101549c84927aa54f0be1cf1522
Kazuhiro Sera <seratch@gmail.com>
2013-09-24 21:22:34 +0900

    version 0.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import ScalateKeys._
 object SkinnyFrameworkBuild extends Build {
 
   val Organization = "com.github.seratch"
-  val Version = "0.1.0-SNAPSHOT"
+  val Version = "0.1.0"
   val ScalatraVersion = "2.2.1"
   val ScalikeJDBCVersion = "1.6.9-SNAPSHOT"
 
