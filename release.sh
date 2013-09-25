946189ac8f6b0b26c29a2f2ff83675d6e20144ae
Kazuhiro Sera <seratch@gmail.com>
2013-09-25 14:32:39 +0900

    Fixed deploy scripts

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sbt ++2.10.0 \
-  orm/publish \
-  validator/publish \
-  framework/publish \
-  freemarker/publish 
+  orm/publish-signed \
+  validator/publish-signed \
+  framework/publish-signed \
+  freemarker/publish-signed 
 
