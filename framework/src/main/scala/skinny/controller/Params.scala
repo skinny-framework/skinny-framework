2fb08e3a46e35a3e22a3ccfba4660409022d05cf
Kazuhiro Sera <seratch@gmail.com>
2013-10-05 13:17:51 +0900

    Flattened optional values from params

--- framework/src/main/scala/skinny/controller/Params.scala
+++ framework/src/main/scala/skinny/controller/Params.scala
@@ -4,6 +4,12 @@ import scala.language.dynamics
 
 case class Params(underlying: Map[String, Any]) extends Dynamic {
 
-  def selectDynamic(name: String): Option[Any] = underlying.get(name)
+  def selectDynamic(name: String): Option[Any] = underlying.get(name).map { v =>
+    v match {
+      case Some(v) => v
+      case None => null
+      case v => v
+    }
+  }
 
 }
