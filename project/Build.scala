aa3d23066ebeee9d84ef39b0d7fbde6978491ee3
Kazuhiro Sera <seratch@gmail.com>
2013-10-19 08:03:28 +0900

    version 0.9.4

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import ScalateKeys._
 object SkinnyFrameworkBuild extends Build {
 
   val Organization = "com.github.seratch"
-  val Version = "0.9.4-SNAPSHOT"
+  val Version = "0.9.4"
   val ScalatraVersion = "2.2.1"
   val Json4SVersion = "3.2.5"
   val ScalikeJDBCVersion = "1.6.10"
--- yeoman-generator-skinny/app/templates/project/Build.scala
+++ yeoman-generator-skinny/app/templates/project/Build.scala
@@ -5,7 +5,7 @@ import org.scalatra.sbt.PluginKeys._
 
 object SkinnyAppBuild extends Build {
 
-  val skinnyVersion = "0.9.3"
+  val skinnyVersion = "0.9.4"
   val scalatraVersion = "2.2.1"
 
   lazy val app = Project (id = "app", base = file("."),
@@ -15,14 +15,13 @@ object SkinnyAppBuild extends Build {
       version := "0.0.1-SNAPSHOT",
       scalaVersion := "2.10.3",
       resolvers ++= Seq(
-        "sonatype releases"  at "http://oss.sonatype.org/content/repositories/releases",
-        "sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
+        "sonatype releases"  at "http://oss.sonatype.org/content/repositories/releases"
       ),
       libraryDependencies ++= Seq(
         "com.github.seratch" %% "skinny-framework"   % skinnyVersion,
         "com.h2database"     %  "h2"                 % "1.3.173", // your JDBC driver
-        "com.github.seratch" %% "skinny-test"        % skinnyVersion   % "test",
-        "org.scalatra"       %% "scalatra-scalatest" % scalatraVersion % "test",
+        "com.github.seratch" %% "skinny-test"        % skinnyVersion         % "test",
+        "org.scalatra"       %% "scalatra-scalatest" % scalatraVersion       % "test",
         "ch.qos.logback"     %  "logback-classic"    % "1.0.13",
         "org.eclipse.jetty"  %  "jetty-webapp"       % "8.1.13.v20130916"    % "container",
         "org.eclipse.jetty"  %  "jetty-plus"         % "8.1.13.v20130916"    % "container",
