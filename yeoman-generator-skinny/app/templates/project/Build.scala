5e35948b7fbdf4ce80c611886b5341a0155a0e20
Kazuhiro Sera <seratch@gmail.com>
2013-11-10 00:51:33 +0900

    0.9.13-SNAPSHOT

--- yeoman-generator-skinny/app/templates/project/Build.scala
+++ yeoman-generator-skinny/app/templates/project/Build.scala
@@ -7,7 +7,7 @@ import ScalateKeys._
 
 object SkinnyAppBuild extends Build {
 
-  val skinnyVersion = "0.9.12"
+  val skinnyVersion = "0.9.13-SNAPSHOT"
   val scalatraVersion = "2.2.1"
   val _scalaVersion = "2.10.3"
 
