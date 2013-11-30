296858d6e0ea9b430ced6cc8a7dc22d6a503d6f4
Kazuhiro Sera <seratch@gmail.com>
2013-11-30 22:33:19 +0900

    version 0.9.18
     - Changed AssetsController to be extensible
     - #32 Improve scaffold generator's validation message
     - #33 IDE settings support by default in skinny-blank-app
     - #34 Enable dynamically replacing tableName for models
     - Scalatra 2.2.2, ScalikeJDBC 1.7.1

--- yeoman-generator-skinny/app/templates/project/Build.scala
+++ yeoman-generator-skinny/app/templates/project/Build.scala
@@ -7,7 +7,7 @@ import ScalateKeys._
 
 object SkinnyAppBuild extends Build {
 
-  val skinnyVersion = "0.9.18-SNAPSHOT"
+  val skinnyVersion = "0.9.18"
   val scalatraVersion = "2.2.2"
   val _scalaVersion = "2.10.3"
   val jettyVersion = "8.1.14.v20131031"
