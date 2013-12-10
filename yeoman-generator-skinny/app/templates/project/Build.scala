ee7cfdc36098e894c80920361023bf41e1ff20e9
Kazuhiro Sera <seratch@gmail.com>
2013-12-10 16:55:33 +0900

    version 0.9.20
     - Enabled specifying ordering in Skinny ORM Finder/Querying APIs
     - Added JSONFeature to provide handy JSON APIs
     - Added explicit redirect{301, 302, 303} methods
     - Added Reverse-routes support

--- yeoman-generator-skinny/app/templates/project/Build.scala
+++ yeoman-generator-skinny/app/templates/project/Build.scala
@@ -7,7 +7,7 @@ import ScalateKeys._
 
 object SkinnyAppBuild extends Build {
 
-  val skinnyVersion = "0.9.20-SNAPSHOT"
+  val skinnyVersion = "0.9.20"
   val _scalaVersion = "2.10.3"
   val jettyVersion = "8.1.14.v20131031"
 
