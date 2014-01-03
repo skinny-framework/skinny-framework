7d0d17c61ee86367e41cd408f34a3a48678972fc
Kazuhiro Sera <seratch@gmail.com>
2014-01-03 13:48:24 +0900

    Added tests for AssetsController

--- example/src/test/scala/controller/AssetsSpec.scala
+++ example/src/test/scala/controller/AssetsSpec.scala
@@ -12,6 +12,7 @@ class AssetsSpec extends ScalatraFlatSpec with SkinnyTestSupport {
   it should "show coffee script resources" in {
     get("/assets/js/echo.js") {
       status should equal(200)
+      header("Content-Type") should equal("application/javascript;charset=UTF-8")
       body should equal("""(function() {
   var echo;
 
@@ -25,10 +26,16 @@ class AssetsSpec extends ScalatraFlatSpec with SkinnyTestSupport {
 """)
     }
   }
+  it should "return 304 for coffee if If-Modified-Since specified" in {
+    get(uri = "/assets/js/echo.js", headers = Map("If-Modified-Since" -> "Thu, 31 Dec 2037 12:34:56 GMT")) {
+      status should equal(304)
+    }
+  }
 
   it should "show less resources" in {
     get("/assets/css/box.css") {
       status should equal(200)
+      header("Content-Type") should equal("text/stylesheet;charset=UTF-8")
       body should equal(""".box {
   color: #fe33ac;
   border-color: #fdcdea;
@@ -36,22 +43,39 @@ class AssetsSpec extends ScalatraFlatSpec with SkinnyTestSupport {
 """)
     }
   }
+  it should "return 304 for less if If-Modified-Since specified" in {
+    get(uri = "/assets/css/box.css", headers = Map("If-Modified-Since" -> "Thu, 31 Dec 2037 12:34:56 GMT")) {
+      status should equal(304)
+    }
+  }
 
   it should "show scss resources" in {
     get("/assets/css/variables-in-scss.css") {
       status should equal(200)
+      header("Content-Type") should equal("text/stylesheet;charset=UTF-8")
       body should equal("""body {
       |  font: 100% Helvetica, sans-serif;
       |  color: #333333; }""".stripMargin)
+    }
+  }
+  it should "return 304 for scss if If-Modified-Since specified" in {
+    get(uri = "/assets/css/variables-in-scss.css", headers = Map("If-Modified-Since" -> "Thu, 31 Dec 2037 12:34:56 GMT")) {
+      status should equal(304)
     }
   }
 
   it should "show sass resources" in {
     get("/assets/css/indented-sass.css") {
       status should equal(200)
+      header("Content-Type") should equal("text/stylesheet;charset=UTF-8")
       body should equal("""#main {
-       |  color: blue;
-       |  font-size: 0.3em; }""".stripMargin)
+                          |  color: blue;
+                          |  font-size: 0.3em; }""".stripMargin)
+    }
+  }
+  it should "return 304 for sass if If-Modified-Since specified" in {
+    get(uri = "/assets/css/indented-sass.css", headers = Map("If-Modified-Since" -> "Thu, 31 Dec 2037 12:34:56 GMT")) {
+      status should equal(304)
     }
   }
 
