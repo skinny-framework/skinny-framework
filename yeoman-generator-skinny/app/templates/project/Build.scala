94be6d7b80b3772930c0630564f5f3a4dcff1ea4
Kazuhiro Sera <seratch@gmail.com>
2014-01-08 15:46:13 +0900

    Fixed jettyVersion variable

--- yeoman-generator-skinny/app/templates/project/Build.scala
+++ yeoman-generator-skinny/app/templates/project/Build.scala
@@ -11,8 +11,8 @@ object SkinnyAppBuild extends Build {
   val _scalaVersion = "2.10.3"
 
   // In some cases, Jety 9.1 looks very slow (didn't investigate the reason)
-  //val JettyVersion = "9.1.0.v20131115"
-  val JettyVersion = "9.0.7.v20131107"
+  //val jettyVersion = "9.1.0.v20131115"
+  val jettyVersion = "9.0.7.v20131107"
 
   val _resolovers = Seq(
     "sonatype releases"  at "http://oss.sonatype.org/content/repositories/releases"
