3dfe55bab8a8271243943b3b45cd0bb93ed4e2f1
Kazuhiro Sera <seratch@gmail.com>
2014-02-05 01:25:43 +0900

    version 0.9.28
     - #71 Fixed SkinnyEnv.staging's value typo by @cb372
     - #72 Feature/multiple tmpl langs support by @cb372
     - #74 oinsFeature#joins doesn't work when `byDefault` is absent. (reported by @BlackPrincess)
     - Bumped jQuery to 2.1.0, Bootstrap to 3.1.0
     - #70 Tests generated by scaffold generator don't work by default by @Arakaki
     - Fixed that dbSeedsAutoSession in DBSeeds cannot be overridden

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import ScalateKeys._
 object SkinnyFrameworkBuild extends Build {
 
   val _organization = "org.skinny-framework"
-  val _version = "0.9.28-SNAPSHOT"
+  val _version = "0.9.28"
   val scalatraVersion = "2.2.2"
   val json4SVersion = "3.2.6"
   val scalikeJDBCVersion = "1.7.4"
