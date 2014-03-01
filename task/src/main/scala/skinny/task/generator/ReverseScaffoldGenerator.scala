4c41328c6ceb77277aaeff60163827f26341b08c
Kazuhiro Sera <seratch@gmail.com>
2014-03-01 23:30:37 +0900

    Refactored reverse-scaffold generator, ParamType.BigDecimal support

--- task/src/main/scala/skinny/task/generator/ReverseScaffoldGenerator.scala
+++ task/src/main/scala/skinny/task/generator/ReverseScaffoldGenerator.scala
@@ -2,18 +2,9 @@ package skinny.task.generator
 
 import skinny._
 import scalikejdbc._
-import java.sql.{
-  Connection,
-  Statement,
-  ResultSet,
-  SQLException,
-  DatabaseMetaData,
-  ResultSetMetaData
-}
+import java.sql.{ ResultSet, Connection, SQLException }
+import java.util.Locale
 
-/**
- * Skinny Reverse Generator Task.
- */
 object ReverseScaffoldGenerator extends ReverseScaffoldGenerator
 
 /**
@@ -21,124 +12,154 @@ object ReverseScaffoldGenerator extends ReverseScaffoldGenerator
  */
 trait ReverseScaffoldGenerator extends CodeGenerator {
 
-  private def showUsage = {
+  case class Column(
+    name: String,
+    dataType: Int,
+    typeName: String,
+    size: Int,
+    decimalDigits: Option[Int],
+    nullable: Option[Boolean])
+
+  case class ParamTypeAndNullable(
+    paramType: ParamType,
+    nullable: Boolean)
+
+  protected def showUsage = {
     showSkinnyGenerator()
-    println("""  Usage: sbt "task/run generate:reverse-scaffold table_name Class" """)
-    println("")
+    println("""  Usage: sbt "task/run generate:reverse-scaffold table_name resources resource" """)
+    println()
   }
 
-  private def showErrors(messages: Seq[String]) = {
-    showSkinnyGenerator()
-    println("""  Command failed!""")
-    println("")
-    println(messages.mkString("  Error: ", "\n", "\n"))
-  }
-
-  override protected def showSkinnyGenerator(): Unit = {
+  protected override def showSkinnyGenerator() = {
     println("""
- *** Skinny Reverse Generator Task ***
+ *** Skinny Reverse Engineering Task ***
 """)
   }
 
   def run(templateType: String, args: List[String]): Unit = {
-    if (args.size < 2) {
+    if (args.size < 3) {
       showUsage
       return
     }
-    val tableName = args(0)
-    val className = args(1)
-    val env = if (args.size >= 3) args(2) else "development"
-    showSkinnyGenerator
-    val columnInfos = extractColumnInfos(env, tableName)
-    val nameAndParamTypes = columnInfos.map(SqlTypeMapping.toNameAndParamType)
-    val params: List[String] = List(tableName, className) ++ nameAndParamTypes.filter {
-      // filter the parameters not to pass to ScaffoldGenerator
-      case "id:Long" | "id:Option[Long]" => false
-      case _ => true
+    try {
+      val (tableName, resources, resource) = (args(0), args(1), args(2))
+
+      val skinnyEnv = if (args.size >= 4) args(3) else SkinnyEnv.Development
+      val fields = extractColumns(skinnyEnv, tableName)
+        .map(column => SqlTypeMapping.toScaffoldFieldDef(column))
+        .filter(param => param != "id:Long" && param != "id:Option[Long]")
+        .map(param => toCamelCase(param))
+
+      println(s"""
+              | *** Skinny Reverse Engineering Task ***
+              |
+              |  Table: ${tableName}
+              |  Resources: ${resources}
+              |  Resource: ${resource}
+              |
+              |  Columns:
+              |${fields.map(f => s"   - ${f}").mkString("\n")}""".stripMargin)
+
+      val generator = templateType match {
+        case "ssp" => new ScaffoldSspGenerator {
+          override def withTimestamps: Boolean = false
+          override def skipDBMigration = true
+        }
+        case "scaml" => new ScaffoldScamlGenerator {
+          override def withTimestamps: Boolean = false
+          override def skipDBMigration = true
+        }
+        case "jade" => new ScaffoldJadeGenerator {
+          override def withTimestamps: Boolean = false
+          override def skipDBMigration = true
+        }
+        case _ => throw new IllegalArgumentException("unknown template type: " + templateType)
+      }
+      generator.run(Seq(resources, resource) ++ fields)
+
+    } catch {
+      case e: Exception =>
+        showErrors(Seq(e.getMessage))
     }
-    println()
-    println("  params:")
-    params.foreach(x => println("    " + x))
-    println()
-    type xx = ScaffoldSspGenerator
-    val g = templateType match {
-      case "ssp" => new ScaffoldSspGenerator { override def withTimestamps: Boolean = false }
-      case "scaml" => new ScaffoldScamlGenerator { override def withTimestamps: Boolean = false }
-      case "jade" => new ScaffoldJadeGenerator { override def withTimestamps: Boolean = false }
-      case x => throw new IllegalArgumentException("unknown template type: " + x)
-    }
-    g.run(params)
   }
 
-  def extractColumnInfos(targetEnv: String, tableName: String): ColumnInfos =
-    using(getConnection(targetEnv)) { implicit conn =>
-      Seq(
-        () => extractColumnInfosByQuery(tableName),
-        () => extractColumnInfosByMetaData(tableName),
-        () => extractColumnInfosByMetaData(tableName.toLowerCase),
-        () => extractColumnInfosByMetaData(tableName.toUpperCase)).foreach { f =>
+  def extractColumns(targetEnv: String, tableName: String): Seq[Column] = {
+    using(borrowConnection(targetEnv)) { implicit conn =>
+      val extractors = Seq(
+        () => extractColumnsByQuery(tableName),
+        () => extractColumnsByMetaData(tableName),
+        () => extractColumnsByMetaData(tableName.toLowerCase),
+        () => extractColumnsByMetaData(tableName.toUpperCase)
+      )
+      extractors.foldLeft[Option[Seq[Column]]](None) {
+        case (z, extractor) =>
           try {
-            val result = f()
-            if (!result.isEmpty) return result
+            z.orElse {
+              val columns = extractor.apply()
+              if (columns.isEmpty) z else Some(columns)
+            }
           } catch {
-            case e: SQLException => println("  warn: " + e)
+            case e: SQLException => z
             case e: Throwable => throw e
           }
-        }
-      Nil
-    }
-
-  def extractColumnInfosByQuery(tableName: String)(implicit conn: Connection): ColumnInfos =
-    using(conn.createStatement) { stmt =>
-      val sql = s"SELECT * FROM ${tableName} LIMIT 0"
-      val m = stmt.executeQuery(sql).getMetaData
-      (1 to m.getColumnCount).toList.map { i =>
-        val decimalDigitsOpt = Some(m.getScale(i))
-        val nullableOpt = toOptionalNullable(m.isNullable(i))
-        ColumnInfo(m.getColumnName(i), m.getColumnType(i),
-          m.getColumnTypeName(i), m.getPrecision(i), decimalDigitsOpt, nullableOpt)
+      }.getOrElse {
+        throw new IllegalStateException(s"Failed to retrieve meta data about columns for ${tableName}")
       }
     }
+  }
 
-  def extractColumnInfosByMetaData(tableName: String)(implicit conn: Connection): ColumnInfos =
+  def extractColumnsByQuery(tableName: String)(implicit conn: Connection): Seq[Column] = {
+    using(conn.createStatement) { stmt =>
+      val meta = stmt.executeQuery(s"SELECT * FROM ${tableName} LIMIT 0").getMetaData
+      (1 to meta.getColumnCount).map { i =>
+        Column(
+          name = meta.getColumnName(i),
+          dataType = meta.getColumnType(i),
+          typeName = meta.getColumnTypeName(i),
+          size = meta.getPrecision(i),
+          decimalDigits = Option(meta.getScale(i)),
+          nullable = extractNullable(meta.isNullable(i))
+        )
+      }
+    }
+  }
+
+  def extractColumnsByMetaData(tableName: String)(implicit conn: Connection): Seq[Column] =
     using(conn.getMetaData.getColumns(null, null, tableName, null)) { rs =>
-      def f(infos: ColumnInfos): ColumnInfos =
+      def extract(rs: ResultSet, columns: Seq[Column]): Seq[Column] = {
         if (rs.next) {
-          val decimalDigitsOpt = rs.getInt(9) match {
-            case _ if rs.wasNull => None
-            case x => Some(x)
-          }
-          val nullableOpt = rs.getInt(11) match {
-            case _ if rs.wasNull => None
-            case x => toOptionalNullable(x)
-          }
-          f(ColumnInfo(
-            rs.getString(4), rs.getInt(5), rs.getString(6),
-            rs.getInt(7), decimalDigitsOpt, nullableOpt) :: infos)
-        } else infos
-      return f(Nil).reverse
+          extract(rs, {
+            columns :+ Column(
+              name = rs.getString(4),
+              dataType = rs.getInt(5),
+              typeName = rs.getString(6),
+              size = rs.getInt(7),
+              decimalDigits = rs.getInt(9) match {
+                case _ if rs.wasNull => None
+                case digits => Some(digits)
+              },
+              nullable = rs.getInt(11) match {
+                case _ if rs.wasNull => None
+                case nullable => extractNullable(nullable)
+              })
+          })
+        } else columns
+      }
+      extract(rs, Nil)
     }
 
-  def toOptionalNullable(v: Int): Option[Boolean] = v match {
+  // None: unknown
+  protected def extractNullable(v: Int): Option[Boolean] = v match {
     case 0 /* attributeNoNulls */ => Some(false)
     case 1 /* attributeNullable */ => Some(true)
     case 2 /* attributeNullableUnknown */ => None
     case _ => None
   }
 
-  def getConnection(env: String): Connection = {
-    val x = scalikejdbc.config.TypesafeConfigReaderWithEnv(env).readJDBCSettings()
-    CommonsConnectionPoolFactory(x.url, x.user, x.password).borrow
+  protected def borrowConnection(env: String): Connection = {
+    val jdbc = scalikejdbc.config.TypesafeConfigReaderWithEnv(env).readJDBCSettings()
+    CommonsConnectionPoolFactory(jdbc.url, jdbc.user, jdbc.password).borrow
   }
-
-  /**
-   * A structure of database column info.
-   */
-  case class ColumnInfo(
-    name: String, dataType: Int, typeName: String, size: Int,
-    decimalDigits: Option[Int], nullable: Option[Boolean])
-
-  type ColumnInfos = List[ColumnInfo]
 
   /**
    * SQL type mapping utility.
@@ -148,60 +169,41 @@ trait ReverseScaffoldGenerator extends CodeGenerator {
     import skinny.ParamType._
     import java.sql.Types._
 
-    def toNameAndParamType(info: ColumnInfo): String = {
-      val optionableParamType = toOptionableParamType(info)
-      val paramType = optionableParamType match {
-        case Some(x) => x
-        case x => x
+    def toScaffoldFieldDef(column: Column): String = {
+      val ParamTypeAndNullable(paramType, nullable) = toParamTypeAndNullable(column)
+      val paramTypeString: String = if (nullable) s"Option[$paramType]" else paramType.toString
+      val columnTypeString: String = {
+        paramType match {
+          case String => s":varchar(${column.size})"
+          case BigDecimal => s":number(${column.size})"
+          case _ => ""
+        }
       }
-      val paramTypeName = paramType.getClass.getSimpleName.replace("$", "")
-      val paramTypeString = (paramTypeName, optionableParamType) match {
-        case (x, Some(_)) => s"Option[$x]"
-        case (x, _) => x
-      }
-      val sqlTypeOpt: Option[String] = (paramType, info) match {
-        case (String | Some(String), x) => Option("varchar(" + x.size + ")")
-        case (BigDecimal, x) => Option("NUMBER(" + x.size + ")")
-        case _ => None
-      }
-      val sqlTypeString = sqlTypeOpt match {
-        case Some(x) => ":" + x
-        case _ => ""
-      }
-      info.name.toLowerCase + ":" + paramTypeString + sqlTypeString
+      toCamelCase(column.name.toLowerCase(Locale.ENGLISH)) + ":" + paramTypeString + columnTypeString
     }
 
-    def toOptionableParamType(info: ColumnInfo): Any = {
-      // add custom mappings if necessary
-      jdbcSqlTypeToParamType(info.dataType) match {
-        case x if info.nullable.getOrElse(true) => Option(x)
-        case x => x
-      }
+    def toParamTypeAndNullable(column: Column): ParamTypeAndNullable = {
+      ParamTypeAndNullable(
+        paramType = convertJdbcSqlTypeToParamType(column.dataType),
+        nullable = column.nullable.getOrElse(true)
+      )
     }
 
-    def jdbcSqlTypeToParamType(dataType: Int): Any = dataType match {
+    def convertJdbcSqlTypeToParamType(dataType: Int): ParamType = dataType match {
       case CHAR | VARCHAR | LONGVARCHAR | LONGNVARCHAR | NCHAR | NVARCHAR | CLOB | NCLOB => String
       case BOOLEAN | BIT => Boolean
       case TINYINT => Byte
       case SMALLINT => Short
       case INTEGER => Int
       case BIGINT => Long
-      case REAL => Float
-      case DOUBLE | FLOAT => Double
-      case DECIMAL | NUMERIC => BigDecimal
+      case FLOAT | REAL => Float
+      case DOUBLE => Double
+      case NUMERIC | DECIMAL => BigDecimal
       case DATE => LocalDate
       case TIME => LocalTime
       case TIMESTAMP => DateTime
       case BINARY | VARBINARY | LONGVARBINARY | BLOB => ByteArray
-      case ARRAY => "Array"
-      case DATALINK => "DataLink"
-      case DISTINCT => "Distinct"
-      case JAVA_OBJECT => "JavaObject"
-      case NULL => "Null"
-      case OTHER => "Other"
-      case REF => "Ref"
-      case ROWID => "RowId"
-      case STRUCT => "Struct"
+      case _ => String
     }
 
   }
