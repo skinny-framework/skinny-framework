04be45d0942af8857cafaba12f0ae27571799e14
Kazuhiro Sera <seratch@gmail.com>
2014-03-04 11:17:37 +0900

    Bumped H2 version

--- yeoman-generator-skinny/app/templates/project/Build.scala
+++ yeoman-generator-skinny/app/templates/project/Build.scala
@@ -22,7 +22,7 @@ object SkinnyAppBuild extends Build {
       "org.skinny-framework"    %% "skinny-framework"   % skinnyVersion,
       "org.skinny-framework"    %% "skinny-assets"      % skinnyVersion,
       "org.skinny-framework"    %% "skinny-task"        % skinnyVersion,
-      "com.h2database"          %  "h2"                 % "1.3.174",      // your own JDBC driver
+      "com.h2database"          %  "h2"                 % "1.3.175",      // your own JDBC driver
       "ch.qos.logback"          %  "logback-classic"    % "1.1.1",
       // To fix java.lang.ClassNotFoundException: scala.collection.Seq when running tests
       "org.scala-lang"          %  "scala-library"      % "2.10.3"              % "test",
