b5fcc72816ea004512f162e4872cfb64375bde32
Kazuhiro Sera <seratch@gmail.com>
2014-03-09 23:08:00 +0900

    Set singleLineMode logging as default

--- yeoman-generator-skinny/app/templates/src/main/scala/ScalatraBootstrap.scala
+++ yeoman-generator-skinny/app/templates/src/main/scala/ScalatraBootstrap.scala
@@ -4,6 +4,11 @@ import _root_.controller._
 
 class ScalatraBootstrap extends SkinnyLifeCycle {
 
+  // If you prefer more logging, configure this settings 
+  scalikejdbc.GlobalSettings.loggingSQLAndTime = scalikejdbc.LoggingSQLAndTimeSettings(
+    singleLineMode = true
+  )
+
   override def initSkinnyApp(ctx: ServletContext) {
     Controllers.root.mount(ctx)
     AssetsController.mount(ctx)
