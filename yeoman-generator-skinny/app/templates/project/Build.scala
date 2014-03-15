9098ea62c73623c1d2f6be2126291048d1a9f881
Kazuhiro Sera <seratch@gmail.com>
2014-03-16 08:23:17 +0900

    Fixed Travis error

--- yeoman-generator-skinny/app/templates/project/Build.scala
+++ yeoman-generator-skinny/app/templates/project/Build.scala
@@ -8,7 +8,7 @@ import scala.language.postfixOps
 
 object SkinnyAppBuild extends Build {
 
-  val skinnyVersion = "1.0.0-RC11"
+  val skinnyVersion = "1.0.0-RC11-SNAPSHOT"
   val scalatraVersion = "2.2.2"
 
   // We choose Jetty 8 as default for Java 6(!) users. 
