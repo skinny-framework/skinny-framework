11eaf25bb7673579d60a8652c1d06585a97f8fef
Kazuhiro Sera <seratch@gmail.com>
2014-03-15 12:07:03 +0900

    Refactored SkinnyResource routes

--- framework/src/main/scala/skinny/controller/SkinnyResourceRoutes.scala
+++ framework/src/main/scala/skinny/controller/SkinnyResourceRoutes.scala
@@ -23,73 +23,69 @@ trait SkinnyApiResourceRoutes[Id] extends SkinnyController with Routes { self: S
   private[this] implicit val skinnyController: SkinnyController = this
 
   // --------------
-  // create
+  // create API
+  val createApiUrl = post(s"${resourcesBasePath}.:ext")(createApiAction).as('createApi)
+  @deprecated("Use createApiUrl instead", since = "1.0.0")
+  val createExtUrl = createApiUrl
 
-  val createUrl = post(s"${resourcesBasePath}")(createResource).as('create)
-  val createWithSlashUrl = post(s"${resourcesBasePath}/")(createResource).as('createWithSlash)
-  val createExtUrl = post(s"${resourcesBasePath}.:ext") {
-    params.get("ext").map {
-      case "json" => createResource()(Format.JSON)
-      case "xml" => createResource()(Format.XML)
-      case _ => haltWithBody(404)
-    } getOrElse haltWithBody(404)
-  }.as('createWithExt)
+  def createApiAction = params.get("ext").map {
+    case "json" => createResource()(Format.JSON)
+    case "xml" => createResource()(Format.XML)
+    case _ => haltWithBody(404)
+  } getOrElse haltWithBody(404)
 
   // --------------
-  // show
+  // index API
 
-  val indexExtUrl: Route = routeForIndexApi
+  val indexApiUrl: Route = get(s"${resourcesBasePath}.:ext")(indexApiAction).as('index)
+  @deprecated("Use indexApiUrl instead", since = "1.0.0")
+  val indexExtUrl = indexApiUrl
 
-  protected def routeForIndexApi = get(s"${resourcesBasePath}.:ext") {
-    (for {
-      ext <- params.get("ext")
-    } yield {
-      ext match {
-        case "json" => showResources()(Format.JSON)
-        case "xml" => showResources()(Format.XML)
-        case _ => haltWithBody(404)
-      }
-    }) getOrElse haltWithBody(404)
-  }.as('index)
+  protected def indexApiAction = (for {
+    ext <- params.get("ext")
+  } yield {
+    ext match {
+      case "json" => showResources()(Format.JSON)
+      case "xml" => showResources()(Format.XML)
+      case _ => haltWithBody(404)
+    }
+  }) getOrElse haltWithBody(404)
+
+  // --------------
+  // show API
 
   val showApiUrl: Route = routeForShowApi
   @deprecated("Use showApiUrl instead", since = "1.0.0")
   val showExtUrl = showApiUrl
 
-  protected def routeForShowApi = get(s"${resourcesBasePath}/:${idParamName}.:ext") {
-    (for {
-      id <- params.getAs[Id](idParamName)
-      ext <- params.get("ext")
-    } yield {
-      ext match {
-        case "json" => showResource(id)(Format.JSON)
-        case "xml" => showResource(id)(Format.XML)
-        case _ => haltWithBody(404)
-      }
-    }) getOrElse haltWithBody(404)
-  }.as('show)
+  protected def routeForShowApi = {
+    get(s"${resourcesBasePath}/:${idParamName}.:ext")(showApiAction).as('showApi)
+  }
+  protected def showApiAction = (for {
+    id <- params.getAs[Id](idParamName)
+    ext <- params.get("ext")
+  } yield {
+    ext match {
+      case "json" => showResource(id)(Format.JSON)
+      case "xml" => showResource(id)(Format.XML)
+      case _ => haltWithBody(404)
+    }
+  }) getOrElse haltWithBody(404)
 
   // --------------
-  // update
+  // update API
 
-  val updatePostUrl = post(s"${resourcesBasePath}/:${idParamName}")(updateAction).as('update)
-  val updatePostApiUrl = post(s"${resourcesBasePath}/:${idParamName}.:ext")(updateApiAction).as('updateWithExt)
+  val updatePostApiUrl = post(s"${resourcesBasePath}/:${idParamName}.:ext")(updateApiAction).as('updateApi)
   @deprecated("Use updatePostApiUrl instead", since = "1.0.0")
   val updatePostExtUrl = updatePostApiUrl
 
-  val updateUrl = put(s"${resourcesBasePath}/:${idParamName}")(updateAction).as('update)
-  val updatePutApiUrl = put(s"${resourcesBasePath}/:${idParamName}.:ext")(updateApiAction).as('updateWithExt)
+  val updatePutApiUrl = put(s"${resourcesBasePath}/:${idParamName}.:ext")(updateApiAction).as('updateApi)
   @deprecated("Use updatePutApiUrl instead", since = "1.0.0")
   val updatePutExtUrl = updatePutApiUrl
 
-  val updatePatchUrl = patch(s"${resourcesBasePath}/:${idParamName}")(updateAction).as('update)
-  val updatePatchApiUrl = patch(s"${resourcesBasePath}/:${idParamName}.:ext")(updateApiAction).as('updateWithExt)
+  val updatePatchApiUrl = patch(s"${resourcesBasePath}/:${idParamName}.:ext")(updateApiAction).as('updateApi)
   @deprecated("Use updatePatchApiUrl instead", since = "1.0.0")
   val updatePatchExtUrl = updatePatchApiUrl
-
-  protected def updateAction = {
-    params.getAs[Id](idParamName).map(id => updateResource(id)) getOrElse haltWithBody(404)
-  }
 
   protected def updateApiAction = {
     (for {
@@ -105,17 +101,9 @@ trait SkinnyApiResourceRoutes[Id] extends SkinnyController with Routes { self: S
   }
 
   // --------------
-  // delete
+  // delete API
 
-  val destroyUrl = delete(s"${resourcesBasePath}/:${idParamName}")(deleteAction).as('destroy)
-  @deprecated("Use destroyUrl instead", since = "1.0.0")
-  val deleteUrl = destroyUrl
-
-  protected def deleteAction = {
-    params.getAs[Id](idParamName).map(id => destroyResource(id)) getOrElse haltWithBody(404)
-  }
-
-  val destroyApiUrl = delete(s"${resourcesBasePath}/:${idParamName}.:ext")(deleteApiAction).as('destroyWithExt)
+  val destroyApiUrl = delete(s"${resourcesBasePath}/:${idParamName}.:ext")(deleteApiAction).as('destroyApi)
   @deprecated("Use destroyApiUrl instead", since = "1.0.0")
   val destroyExtUrl = destroyApiUrl
   @deprecated("Use destroyApiUrl instead", since = "1.0.0")
@@ -147,7 +135,7 @@ trait SkinnyResourceRoutes[Id] extends SkinnyApiResourceRoutes[Id] { self: Skinn
   // show
 
   val indexUrl = get(s"${resourcesBasePath}")(showResources()).as('index)
-  val indexWithSlashUrl = get(s"${resourcesBasePath}/")(showResources()).as('indexWithSlash)
+  val indexWithSlashUrl = get(s"${resourcesBasePath}/")(showResources()).as('index)
 
   val showUrl = get(s"${resourcesBasePath}/:${idParamName}") {
     params.getAs[Id](idParamName).map(id => showResource(id)).getOrElse(haltWithBody(404))
@@ -163,11 +151,33 @@ trait SkinnyResourceRoutes[Id] extends SkinnyApiResourceRoutes[Id] { self: Skinn
   // Scalatra takes priority to route definition which is defined later.
   val newUrl = get(s"${resourcesBasePath}/new")(newResource).as('new)
 
+  val createUrl = post(s"${resourcesBasePath}")(createResource).as('create)
+  val createWithSlashUrl = post(s"${resourcesBasePath}/")(createResource).as('create)
+
   // --------------
   // update
 
   val editUrl = get(s"${resourcesBasePath}/:${idParamName}/edit") {
     params.getAs[Id](idParamName).map(id => editResource(id)) getOrElse haltWithBody(404)
   }.as('edit)
+
+  val updatePostUrl = post(s"${resourcesBasePath}/:${idParamName}")(updateAction).as('update)
+  val updateUrl = put(s"${resourcesBasePath}/:${idParamName}")(updateAction).as('update)
+  val updatePatchUrl = patch(s"${resourcesBasePath}/:${idParamName}")(updateAction).as('update)
+
+  protected def updateAction = {
+    params.getAs[Id](idParamName).map(id => updateResource(id)) getOrElse haltWithBody(404)
+  }
+
+  // --------------
+  // delete
+
+  val destroyUrl = delete(s"${resourcesBasePath}/:${idParamName}")(deleteAction).as('destroy)
+  @deprecated("Use destroyUrl instead", since = "1.0.0")
+  val deleteUrl = destroyUrl
+
+  protected def deleteAction = {
+    params.getAs[Id](idParamName).map(id => destroyResource(id)) getOrElse haltWithBody(404)
+  }
 
 }
