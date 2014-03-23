27a584ebacd500d552f879322d24c164ece5fc1d
Kazuhiro Sera <seratch@gmail.com>
2014-03-24 00:35:32 +0900

    SNAPSHOT

--- yeoman-generator-skinny/app/templates/project/Build.scala
+++ yeoman-generator-skinny/app/templates/project/Build.scala
@@ -12,7 +12,7 @@ object SkinnyAppBuild extends Build {
   // Common Settings
   // -------------------------------------------------------
 
-  val skinnyVersion = "1.0.0-RC12"
+  val skinnyVersion = "1.0.0-SNAPSHOT"
   val scalatraVersion = "2.2.2"
 
   // We choose Jetty 8 as default for Java 6(!) users. 
