348f78f7d88419cdbab5c340cf98500699f8eef1
Kazuhiro Sera <seratch@gmail.com>
2014-05-02 01:28:54 +0900

    version 1.0.12
     - [orm] scalikejdbc 1.7.7 to speed up compliation time
     - [factory-girl] twitter/util-eval to 6.15.0

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import ScalateKeys._
 object SkinnyFrameworkBuild extends Build {
 
   val _organization = "org.skinny-framework"
-  val _version = "1.0.11"
+  val _version = "1.0.12"
   val scalatraVersion = "2.2.2"
   val json4SVersion = "3.2.9"
   val scalikeJDBCVersion = "1.7.7"
