13a87c7a3f671c3c77535426ead117cf15e431a9
Kazuhiro Sera <seratch@gmail.com>
2014-05-12 19:26:36 +0900

    Added test code which demonstrates that hasManyThrough byDefault each other is not supported

---
+++ orm/src/test/scala/test001/Connection.scala
@@ -0,0 +1,8 @@
+package test001
+
+import scalikejdbc.ConnectionPool
+
+trait Connection {
+  Class.forName("org.h2.Driver")
+  ConnectionPool.add('test001, "jdbc:h2:mem:test001;MODE=PostgreSQL", "sa", "sa")
+}
