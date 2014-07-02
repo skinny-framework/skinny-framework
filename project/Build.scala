d4524ff15edd25714f081eb74cc3c19a889c8623
Kazuhiro Sera <seratch@gmail.com>
2014-07-03 01:28:22 +0900

    version 1.1.4
     - [scaldi] Enable using Scaldi by configuration

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import ScalateKeys._
 object SkinnyFrameworkBuild extends Build {
 
   val _organization = "org.skinny-framework"
-  val _version = "1.1.3"
+  val _version = "1.1.4"
   val scalatraVersion = "2.3.0"
   val json4SVersion = "3.2.10"
   val scalikeJDBCVersion = "2.0.4"
