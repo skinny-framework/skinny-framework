807c633c692880ec5c9b45d008db264b595416df
Kazuhiro Sera <seratch@gmail.com>
2014-07-29 18:52:10 +0900

    Update ScalatraBootstrap in skinny-blank-app

--- yeoman-generator-skinny/app/templates/src/main/scala/ScalatraBootstrap.scala
+++ yeoman-generator-skinny/app/templates/src/main/scala/ScalatraBootstrap.scala
@@ -5,18 +5,21 @@ import _root_.controller._
 class ScalatraBootstrap extends SkinnyLifeCycle {
 
   // If you prefer more logging, configure this settings 
+  // http://scalikejdbc.org/documentation/configuration.html
   scalikejdbc.GlobalSettings.loggingSQLAndTime = scalikejdbc.LoggingSQLAndTimeSettings(
     singleLineMode = true
   )
 
   // simple worker example
+  /*
   val sampleWorker = new skinny.worker.SkinnyWorker with Logging {
     def execute = logger.info("sample worker is called!")
   }
+  */
 
   override def initSkinnyApp(ctx: ServletContext) {
     // http://skinny-framework.org/documentation/worker_jobs.html
-    //skinnyWorkerService.everyFixedSeconds(worker, 3)
+    //skinnyWorkerService.everyFixedSeconds(sampleWorker, 3)
 
     Controllers.mount(ctx)
   }
