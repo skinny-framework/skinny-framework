85b2d2dca0e65b0c8ea30c491b17cecedad4d4fb
Kazuhiro Sera <seratch@gmail.com>
2014-10-03 17:53:35 +0900

    Improve sbt script on Windows

--- yeoman-generator-skinny/app/templates/sbt.bat
+++ yeoman-generator-skinny/app/templates/sbt.bat
@@ -1,3 +1,3 @@
 set SCRIPT_DIR=%~dp0
-java -Xmx1024M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -jar "%SCRIPT_DIR%bin\sbt-launch.jar" %*
+java -Xmx1024M -Xss1M -Dinput.encoding=Cp1252 -Dfile.encoding=SJIS -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -jar "%SCRIPT_DIR%bin\sbt-launch.jar" %*
 
