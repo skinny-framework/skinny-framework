189bf577bf2fe7326acfee44ba87c43c895e6bda
Kazuhiro Sera <seratch@gmail.com>
2014-10-17 16:04:40 +0900

    skinny-logback 1.0.1

--- project/Build.scala
+++ project/Build.scala
@@ -33,6 +33,7 @@ object SkinnyFrameworkBuild extends Build {
     incOptions := incOptions.value.withNameHashing(true),
     logBuffered in Test := false,
     javaOptions in Test ++= Seq("-Dskinny.env=test"),
+    scalacOptions += "-target:jvm-1.7",
     pomExtra := _pomExtra
   )
 
@@ -212,7 +213,7 @@ object SkinnyFrameworkBuild extends Build {
   lazy val logback = Project(id = "logback", base = file("logback"),
     settings = baseSettings ++ Seq(
       name             := "skinny-logback",
-      version          := "1.0.0",
+      version          := "1.0.1",
       crossPaths       := false,
       autoScalaLibrary := false,
       libraryDependencies ++= Seq("ch.qos.logback" % "logback-classic" % "1.1.2" % "compile")
