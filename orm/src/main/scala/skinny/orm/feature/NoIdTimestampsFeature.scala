b807ad3c9b2e2d466d9d80a0fa9b36d31e04699c
Kazuhiro Sera <seratch@gmail.com>
2014-10-18 22:40:15 +0900

    Refactor NoIdTimestampFeature refs https://github.com/skinny-framework/skinny-framework/commit/f7cd0a6cbd2c186be1d0633e0d7fba6ccb80ed63

--- orm/src/main/scala/skinny/orm/feature/NoIdTimestampsFeature.scala
+++ orm/src/main/scala/skinny/orm/feature/NoIdTimestampsFeature.scala
@@ -1,25 +1,17 @@
 package skinny.orm.feature
 
+import org.joda.time.DateTime
 import scalikejdbc._
 import skinny.PermittedStrongParameters
-import org.joda.time.DateTime
 
-/**
- * ActiveRecord timestamps feature.
- *
- * @tparam Entity entity
- */
-trait TimestampsFeature[Entity]
-  extends TimestampsFeatureWithId[Long, Entity]
-
-trait TimestampsFeatureWithId[Id, Entity] extends CRUDFeatureWithId[Id, Entity] with TimestampsFeatureBase[Entity] {
+trait NoIdTimestampsFeature[Entity] extends NoIdCUDFeature[Entity] with TimestampsFeatureBase[Entity] {
 
   override protected def namedValuesForCreation(strongParameters: PermittedStrongParameters): Seq[(SQLSyntax, Any)] = {
     val additionalValues = timestampValues(strongParameters.params.contains)
     super.namedValuesForCreation(strongParameters) ++ additionalValues
   }
 
-  override def createWithNamedValues(namedValues: (SQLSyntax, Any)*)(implicit s: DBSession = autoSession): Id = {
+  override def createWithNamedValues(namedValues: (SQLSyntax, Any)*)(implicit s: DBSession = autoSession): Any = {
     val additionalValues = timestampValues(name => namedValues.exists(_._1 == column.field(name)))
     super.createWithNamedValues(namedValues ++ additionalValues: _*)
   }
@@ -31,3 +23,4 @@ trait TimestampsFeatureWithId[Id, Entity] extends CRUDFeatureWithId[Id, Entity] 
   }
 
 }
+
---
+++ orm/src/main/scala/skinny/orm/feature/TimestampsFeatureBase.scala
@@ -0,0 +1,27 @@
+package skinny.orm.feature
+
+import org.joda.time.DateTime
+import scalikejdbc._
+import skinny.orm.SkinnyMapperBase
+
+trait TimestampsFeatureBase[Entity] { self: SkinnyMapperBase[Entity] =>
+
+  /**
+   * createdAt field name.
+   */
+  def createdAtFieldName = "createdAt"
+
+  /**
+   * updatedAt field name.
+   */
+  def updatedAtFieldName = "updatedAt"
+
+  protected def timestampValues(exists: String => Boolean): Seq[(SQLSyntax, Any)] = {
+    val (column, now) = (defaultAlias.support.column, DateTime.now)
+    val builder = List.newBuilder[(SQLSyntax, Any)]
+    if (!exists(createdAtFieldName)) builder += column.field(createdAtFieldName) -> now
+    if (!exists(updatedAtFieldName)) builder += column.field(updatedAtFieldName) -> now
+    builder.result()
+  }
+
+}
