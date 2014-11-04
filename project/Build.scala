b6c22ace9021e541cdd4f261f7e3a5479322e3e0
Kazuhiro Sera <seratch@gmail.com>
2014-11-04 23:12:16 +0900

    scalikejdbc 2.1.4

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object SkinnyFrameworkBuild extends Build {
   lazy val currentVersion = "1.3.4"
   lazy val scalatraVersion = "2.3.0"
   lazy val json4SVersion = "3.2.10"
-  lazy val scalikeJDBCVersion = "2.1.2"
+  lazy val scalikeJDBCVersion = "2.1.4"
   lazy val h2Version = "1.4.182"
   lazy val jettyVersion = "9.2.1.v20140609" // latest "9.2.3.v20140905"
 
