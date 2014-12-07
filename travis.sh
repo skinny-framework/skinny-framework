9aa38fcb66b2abf41ac7ebf8b4c773daba200acb
Kazuhiro Sera <seratch@gmail.com>
2014-12-07 11:34:33 +0900

    Update Travis settings

--- travis.sh
+++ travis.sh
@@ -2,11 +2,8 @@
 if [[ "$TEST_TYPE" == "framework" ]]; then
   gem install sass && 
   sbt "example/run db:migrate test" && 
-  sbt clean coverage test && 
-  sbt coveralls
-fi
-
-if [[ "$TEST_TYPE" == "blank-app" ]]; then
+  sbt clean coverage test
+elif [[ "$TEST_TYPE" == "blank-app" ]]; then
   export SBT_OPTS="" &&  yes|./run_skinny-blank-app_test.sh
 fi
 
