8ff94bbb72696157c95a064afb779ab1229a7df3
Kazuhiro Sera <seratch@gmail.com>
2015-01-03 22:56:30 +0900

    Fix Travis failireu for now

--- framework/src/test/scala/skinny/filter/TxPerRequestFilterSpec.scala
+++ framework/src/test/scala/skinny/filter/TxPerRequestFilterSpec.scala
@@ -1,12 +1,17 @@
 package skinny.filter
 
 import org.scalatra.test.scalatest.ScalatraFlatSpec
+import scalikejdbc.ConnectionPool
 import skinny.SkinnyApiController
 import skinny.routing.Routes
 
 class TxPerRequestFilterSpec extends ScalatraFlatSpec {
 
+  Class.forName("org.h2.Driver")
+  ConnectionPool.add('TxPerRequestFilterSpec, "jdbc:h2:mem:TxPerRequestFilterSpec", "sa", "sa")
+
   object Controller extends SkinnyApiController with TxPerRequestFilter with Routes {
+    override def connectionPoolForTxPerRequestFilter = ConnectionPool.get('TxPerRequestFilterSpec)
     def index = "ok"
     get("/")(index).as('index)
   }
--- orm/src/test/scala/skinny/orm/servlet/TxPerRequestFilterSpec.scala
+++ orm/src/test/scala/skinny/orm/servlet/TxPerRequestFilterSpec.scala
@@ -14,16 +14,14 @@ class TxPerRequestFilterSpec extends FunSpec with Matchers with MockitoSugar {
 
   describe("TxPerRequestFilter") {
     it("should be available") {
-      try {
-        val req = mock[HttpServletRequest]
-        val context = mock[ServletContext]
-        val resp = mock[HttpServletResponse]
-        val chain = mock[FilterChain]
-        when(req.getServletContext).thenReturn(context)
-        when(req.getRequestURI).thenReturn("/")
-        when(context.getContextPath).thenReturn("/")
-        filter.doFilter(req, resp, chain)
-      } catch { case e: Exception => e.printStackTrace() }
+      val req = mock[HttpServletRequest]
+      val context = mock[ServletContext]
+      val resp = mock[HttpServletResponse]
+      val chain = mock[FilterChain]
+      when(req.getServletContext).thenReturn(context)
+      when(req.getRequestURI).thenReturn("/")
+      when(context.getContextPath).thenReturn("/")
+      filter.doFilter(req, resp, chain)
     }
   }
 
