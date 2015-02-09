ce5cb3bc83ca1f283596b237e22207c5747b8115
Kazuhiro Sera <seratch@gmail.com>
2015-02-09 12:05:30 +0900

    version 1.3.13
     - [assets] Fix #235 scss compilation running in production mode by @namutaka
     - [blank-app] Change scalajs:package to use fullOptJS by @seratch

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import scala.language.postfixOps
 
 object SkinnyFrameworkBuild extends Build {
 
-  lazy val currentVersion = "1.3.12"
+  lazy val currentVersion = "1.3.13"
   lazy val scalatraVersion = "2.3.0"
   lazy val json4SVersion = "3.2.11"
   lazy val scalikeJDBCVersion = "2.2.3"
