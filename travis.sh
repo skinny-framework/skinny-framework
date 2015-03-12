d39537aa57d9bdfbbfd7e50f822fc883f127d5f4
Kazuhiro Sera <seratch@gmail.com>
2015-03-12 14:10:32 +0900

    coverage command only for Scala 2.11 on Travis builds

--- travis.sh
+++ travis.sh
@@ -2,7 +2,13 @@
 if [[ "$TEST_TYPE" == "framework" ]]; then
   gem install sass && 
   sbt "example/run db:migrate test" && 
-  sbt clean ++$TRAVIS_SCALA_VERSION coverage test
+  if [[ "$TRAVIS_SCALA_VERSION" == 2.11* ]]; then
+    sbt clean ++$TRAVIS_SCALA_VERSION coverage test
+  else
+    # skip converage for Scala 2.10
+    # https://github.com/scoverage/sbt-scoverage#highlighting
+    sbt clean ++$TRAVIS_SCALA_VERSION test
+  fi
 elif [[ "$TEST_TYPE" == "blank-app" && "$TRAVIS_SCALA_VERSION" == 2.11* ]]; then
   export SBT_OPTS="" &&  yes|./run_skinny-blank-app_test.sh
 fi
