6b68984773ce5c0bdb049de55479a2aa8e9b1b79
Kazuhiro Sera <seratch@gmail.com>
2015-03-21 16:37:20 +0900

    version 1.3.15
    - [assets] Bump builtin compilers (JSXTransformer 0.13.0, CoffeeScript Compiler 1.9.1, LESS 1.7.5)
    - [blank-app] Bump default Scala version to 2.11.6 by @seratch
    - [blank-app] Bump commons-dbcp2 to 2.1 by @seratch
    - [blank-app] Remove migration debug logging by @seratch
    - [blank-app] Bump sbt to 0.13.8 by @seratch
    - [blank-app] Get rid of broken Heroku support by @seratch
    - [blank-app] Update README by @seratch
    - [framework] Add a missing parameter. pageSize(size) by @tsukaby
    - [framework] Bump Scalate to 1.7.1 by @seratch
    - [oauth2-controller] @nulab's Backlog OAuth 2.0 support by @seratch
    - [orm] Fix pagination with hasMany+byDefault doesn't work as expected by @namutaka, @seratch
    - [orm] Remove unnecessary code in association constructor by @seratch
    - [orm] Bump ScalikeJDBC to 2.2.5 by @seratch
    - [orm] Allow specifying AssociationsFeature instance as belongsToWithAliasAndFkAndJoinCondition's right by @seratch

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import scala.language.postfixOps
 
 object SkinnyFrameworkBuild extends Build {
 
-  lazy val currentVersion = "1.3.15-SNAPSHOT"
+  lazy val currentVersion = "1.3.15"
   lazy val scalatraVersion = "2.3.0"
   lazy val json4SVersion = "3.2.11"
   lazy val scalikeJDBCVersion = "2.2.5"
