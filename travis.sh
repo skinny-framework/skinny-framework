a3f99e5f09bf76f3b4cac83484906cb65812345e
Kazuhiro Sera <seratch@gmail.com>
2015-03-23 08:25:53 +0900

    Update travis script

--- travis.sh
+++ travis.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 if [[ "$TEST_TYPE" == "framework" ]]; then
-  gem install sass && 
-  sbt "example/run db:migrate test" && 
   if [[ "$TRAVIS_SCALA_VERSION" == 2.11* ]]; then
+    gem install sass &&
+    sbt "example/run db:migrate test" &&
     sbt clean ++$TRAVIS_SCALA_VERSION coverage test
   else
     # skip converage for Scala 2.10
     # https://github.com/scoverage/sbt-scoverage#highlighting
+    gem install sass &&
+    sbt "example/run db:migrate test" &&
     sbt clean ++$TRAVIS_SCALA_VERSION test
   fi
 elif [[ "$TEST_TYPE" == "blank-app" && "$TRAVIS_SCALA_VERSION" == 2.11* ]]; then
   export SBT_OPTS="" &&  yes|./run_skinny-blank-app_test.sh
 fi
-
