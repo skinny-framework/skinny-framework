44f6d7dd8047d2f328542bb287d41ec51c86bc60
Kazuhiro Sera <seratch@gmail.com>
2015-06-25 00:39:46 +0900

    versin 1.3.19
    - [blank-app] Scala 2.11.7 by @seratch
    - [blank-app] sbt-scalajs 0.6.3 by @seratch
    - [blank-app] #261 Add fork in Test to generated Build.scala by @lloydmeta
    - [common] Bump joda-time, kuromoji minor version by @seratch
    - [orm] #264 Fix #263 findAllByWithLimitOffset doesn't work with child condition by @seratch
    - [orm] ScalikeJDBC 2.2.6 -> 2.2.7 by @seratch

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import scala.language.postfixOps
 
 object SkinnyFrameworkBuild extends Build {
 
-  lazy val currentVersion = "1.3.18"
+  lazy val currentVersion = "1.3.19"
   lazy val scalatraVersion = "2.3.1"
   lazy val json4SVersion = "3.2.11"
   lazy val scalikeJDBCVersion = "2.2.7"
