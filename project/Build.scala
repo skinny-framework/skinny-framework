9969a78055a77f58d1a441a87b39be890c61ad97
Kazuhiro Sera <seratch@gmail.com>
2015-08-19 22:57:07 +0900

    SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import scala.language.postfixOps
 
 object SkinnyFrameworkBuild extends Build {
 
-  lazy val currentVersion = "1.3.20"
+  lazy val currentVersion = "1.3.21-SNAPSHOT"
   lazy val scalatraVersion = "2.3.1"
   lazy val json4SVersion = "3.2.11"
   lazy val scalikeJDBCVersion = "2.2.8"
