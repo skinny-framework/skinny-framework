skinny-framework/skinny-framework

commit 07f4d28c93753eb2b0cb33f314abbceb8c976c20

Skinny Micro 1.0.0.RC1, async before/after filters

framework/src/main/scala/skinny/controller/feature/XContentTypeOptionsNosniffHeaderFeature.scala
@@ -1,6 +1,7 @@
 package skinny.controller.feature
 
 import skinny.micro.SkinnyMicroBase
+import skinny.micro.base.BeforeAfterDsl
 
 /**
  * X-Content-Type-Options header support.
@@ -10,10 +11,9 @@ import skinny.micro.SkinnyMicroBase
  * - https://github.com/blog/1482-heads-up-nosniff-header-support-coming-to-chrome-and-firefox
  * - https://www.owasp.org/index.php/List_of_useful_HTTP_headers
  */
-trait XContentTypeOptionsNosniffHeaderFeature { self: SkinnyMicroBase with BeforeAfterActionFeature =>
+trait XContentTypeOptionsNosniffHeaderFeature { self: SkinnyMicroBase with BeforeAfterDsl =>
 
   // NOTE: To force this header to all responses
-  //beforeAction() {
   before() {
     response(context).setHeader("X-Content-Type-Options", "nosniff")
   }

framework/src/main/scala/skinny/controller/AsyncSkinnyControllerBase.scala
@@ -1,10 +1,10 @@
 package skinny.controller
 
-import skinny.controller.feature.SkinnyControllerCommonBase
+import skinny.controller.feature.{ AsyncBeforeAfterActionFeature, SkinnyControllerCommonBase }
 import skinny.micro.AsyncFeatures
 
 trait AsyncSkinnyControllerBase
     extends SkinnyControllerCommonBase
-    with AsyncFeatures {
-
+    with AsyncFeatures
+    with AsyncBeforeAfterActionFeature {
 }

framework/src/main/scala/skinny/controller/SkinnyControllerBase.scala
@@ -5,6 +5,8 @@ import skinny.micro.ThreadLocalFeatures
 
 trait SkinnyControllerBase
     extends SkinnyControllerCommonBase
-    with ThreadLocalFeatures {
+    with ThreadLocalFeatures
+    with BeforeAfterActionFeature
+    with ThreadLocalRequestFeature {
 
 }

framework/src/main/scala/skinny/controller/feature/AsyncBeforeAfterActionFeature.scala
@@ -1,0 +1,91 @@
+package skinny.controller.feature
+
+import skinny.micro.async.AsyncBeforeAfterDsl
+import skinny.micro._
+
+/**
+ * beforeAction/afterAction support.
+ *
+ * @see http://guides.rubyonrails.org/action_controller_overview.html
+ */
+trait AsyncBeforeAfterActionFeature
+    extends SkinnyMicroBase
+    with AsyncBeforeAfterDsl {
+
+  self: Handler with ActionDefinitionFeature =>
+
+  /**
+   * Collection of beforeAction functions.
+   */
+  private[this] val skinnyBeforeActions = new scala.collection.mutable.ListBuffer[(Context) => Any]
+
+  /**
+   * Collection of afterAction functions.
+   */
+  private[this] val skinnyAfterActions = new scala.collection.mutable.ListBuffer[(Context) => Any]
+
+  /**
+   * If you prefer #beforeFilter than #beforeAction, keep going!
+   */
+  def beforeFilter(only: Seq[Symbol] = Nil, except: Seq[Symbol] = Nil)(action: (Context) => Any): Unit = {
+    beforeAction(only, except)(action)
+  }
+
+  /**
+   * Registers beforeAction to this controller.
+   *
+   * @param only this action should be applied only for these action methods
+   * @param except this action should not be applied for these action methods
+   * @param action action
+   */
+  def beforeAction(only: Seq[Symbol] = Nil, except: Seq[Symbol] = Nil)(action: (Context) => Any): Unit = {
+    skinnyBeforeActions += (
+      (ctx) => {
+        currentActionName.map { name =>
+          // current path should not be excluded
+          if (!except.exists(_ == name)) {
+            // all actions should be included or this action should be included
+            if (only.isEmpty || only.exists(_ == name)) {
+              action(ctx)
+            }
+          }
+        } getOrElse ().asInstanceOf[Any]
+      })
+  }
+
+  /**
+   * If you prefer #afterFilter than #afterAction, keep going!
+   */
+  def afterFilter(only: Seq[Symbol] = Nil, except: Seq[Symbol] = Nil)(action: (Context) => Any): Unit = {
+    afterAction(only, except)(action)
+  }
+
+  /**
+   * Registers afterAction to this controller.
+   *
+   * @param only this action should be applied only for these action methods
+   * @param except this action should not be applied for these action methods
+   * @param action action
+   */
+  def afterAction(only: Seq[Symbol] = Nil, except: Seq[Symbol] = Nil)(action: (Context) => Any): Unit = {
+    skinnyAfterActions += (
+      (ctx) => {
+        currentActionName.map { name =>
+          // current path should not be excluded
+          if (!except.exists(_ == name)) {
+            // all actions should be included or this action should be included
+            if (only.isEmpty || only.exists(_ == name)) {
+              action(ctx)
+            }
+          }
+        } getOrElse ().asInstanceOf[Any]
+      })
+  }
+
+  // executing actions in this controller
+
+  before() { (ctx) => skinnyBeforeActions.foreach(_.apply(ctx)) }
+
+  after() { (ctx) => skinnyAfterActions.foreach(_.apply(ctx)) }
+
+}

framework/src/main/scala/skinny/controller/feature/CSRFProtectionFeature.scala
@@ -1,6 +1,7 @@
 package skinny.controller.feature
 
 import skinny.micro.SkinnyMicroBase
+import skinny.micro.base.BeforeAfterDsl
 import skinny.micro.contrib.CSRFTokenSupport
 import skinny.logging.LoggerProvider
 
@@ -16,7 +17,7 @@ object CSRFProtectionFeature {
  */
 trait CSRFProtectionFeature extends CSRFTokenSupport {
 
-  self: SkinnyMicroBase with ActionDefinitionFeature with BeforeAfterActionFeature with RequestScopeFeature with LoggerProvider =>
+  self: SkinnyMicroBase with ActionDefinitionFeature with BeforeAfterDsl with RequestScopeFeature with LoggerProvider =>
 
   /**
    * Overrides Scalatra's default key name.
@@ -89,7 +90,7 @@ trait CSRFProtectionFeature extends CSRFTokenSupport {
   def handleForgeryIfDetected(): Unit = halt(403)
 
   // Registers csrfKey & csrfToken to request scope.
-  beforeAction() {
+  before() {
     if (getFromRequestScope(RequestScopeFeature.ATTR_CSRF_KEY)(context).isEmpty) {
       set(RequestScopeFeature.ATTR_CSRF_KEY, csrfKey)(context)
       set(RequestScopeFeature.ATTR_CSRF_TOKEN, prepareCsrfToken())(context)

framework/src/main/scala/skinny/controller/feature/SkinnyControllerCommonBase.scala
@@ -28,12 +28,10 @@ trait SkinnyControllerCommonBase
     with ActionDefinitionFeature
     with RequestScopeFeature
     with ChunkedResponseFeature
-    with BeforeAfterActionFeature
     with LocaleFeature
     with JSONFeature
     with ValidationFeature
     with TimeLoggingFeature
-    with ThreadLocalRequestFeature
     with SnakeCasedParamKeysFeature
     with XContentTypeOptionsNosniffHeaderFeature
     with RoutesAsImplicits

framework/src/main/scala/skinny/controller/feature/XFrameOptionsHeaderFeature.scala
@@ -1,13 +1,14 @@
 package skinny.controller.feature
 
 import skinny.micro.SkinnyMicroBase
+import skinny.micro.base.BeforeAfterDsl
 
 /**
  * X-Frame-Options header support
  *
  * - https://www.owasp.org/index.php/List_of_useful_HTTP_headers
  */
-trait XFrameOptionsHeaderFeature { self: SkinnyMicroBase with BeforeAfterActionFeature =>
+trait XFrameOptionsHeaderFeature { self: SkinnyMicroBase with BeforeAfterDsl =>
 
   // NOTE: There are three possible values for the X-Frame-Options headers.
   // - "deny" which prevents any domain from framing the content.
@@ -16,7 +17,7 @@ trait XFrameOptionsHeaderFeature { self: SkinnyMicroBase with BeforeAfterActionF
   lazy val xFrameOptionsPolicy = "sameorigin"
 
   // NOTE: for all HTML responses defined as Skinny routes
-  beforeAction() {
+  before() {
     response(context).setHeader("X-Frame-Options", xFrameOptionsPolicy)
   }
 

framework/src/main/scala/skinny/controller/feature/XXSSProtectionHeaderFeature.scala
@@ -1,16 +1,17 @@
 package skinny.controller.feature
 
 import skinny.micro.SkinnyMicroBase
+import skinny.micro.base.BeforeAfterDsl
 
 /**
  * X-XSS-Protection header support
  *
  * - https://www.owasp.org/index.php/List_of_useful_HTTP_headers
  */
-trait XXSSProtectionHeaderFeature { self: SkinnyMicroBase with BeforeAfterActionFeature =>
+trait XXSSProtectionHeaderFeature { self: SkinnyMicroBase with BeforeAfterDsl =>
 
   // NOTE: for all HTML responses defined as Skinny routes
-  beforeAction() {
+  before() {
     response(context).setHeader("X-XSS-Protection", "1; mode=block")
   }
 

commit cb259f2b0fb241e1b65eeb3bafde59162eefc0e4

Fix regression introduced by 07f4d28c93753eb2b0cb33f314abbceb8c976c20

framework/src/main/scala/skinny/controller/AsyncSkinnyController.scala
@@ -8,4 +8,4 @@ import skinny.micro.AsyncSkinnyMicroFilter
 trait AsyncSkinnyController
   extends AsyncSkinnyMicroFilter
   with AsyncSkinnyControllerBase
-  with SkinnyWebPageControllerFeatures
+  with AsyncSkinnyWebPageControllerFeatures

framework/src/main/scala/skinny/controller/AsyncSkinnyServlet.scala
@@ -8,4 +8,4 @@ import skinny.micro.AsyncSkinnyMicroServlet
 class AsyncSkinnyServlet
   extends AsyncSkinnyMicroServlet
   with AsyncSkinnyControllerBase
-  with SkinnyWebPageControllerFeatures
+  with AsyncSkinnyWebPageControllerFeatures

framework/src/main/scala/skinny/controller/AsyncSkinnyWebPageControllerFeatures.scala
@@ -5,14 +5,14 @@ import skinny.controller.feature._
 /**
  * Additional web pages specific features for SkinnyControllers.
  */
-trait SkinnyWebPageControllerFeatures
+trait AsyncSkinnyWebPageControllerFeatures
     extends SkinnyControllerCommonBase
     with FlashFeature
     with TemplateEngineFeature
     with ScalateTemplateEngineFeature
-    with CSRFProtectionFeature
-    with XXSSProtectionHeaderFeature
-    with XFrameOptionsHeaderFeature { self: BeforeAfterActionFeature =>
+    with AsyncCSRFProtectionFeature
+    with AsyncXXSSProtectionHeaderFeature
+    with AsyncXFrameOptionsHeaderFeature { self: AsyncBeforeAfterActionFeature =>
 
   override def handleForgeryIfDetected() = haltWithBody(403)(context)
 

framework/src/main/scala/skinny/controller/feature/AsyncCSRFProtectionFeature.scala
@@ -4,7 +4,7 @@ import skinny.micro.SkinnyMicroBase
 import skinny.micro.contrib.CSRFTokenSupport
 import skinny.logging.LoggerProvider
 
-object CSRFProtectionFeature {
+object AsyncCSRFProtectionFeature {
 
   // follows Rails default
   val DEFAULT_KEY: String = "csrf-token"
@@ -14,9 +14,9 @@ object CSRFProtectionFeature {
 /**
  * Provides Cross-Site Request Forgery (CSRF) protection.
  */
-trait CSRFProtectionFeature extends CSRFTokenSupport {
+trait AsyncCSRFProtectionFeature extends CSRFTokenSupport {
 
-  self: SkinnyMicroBase with ActionDefinitionFeature with BeforeAfterActionFeature with RequestScopeFeature with LoggerProvider =>
+  self: SkinnyMicroBase with ActionDefinitionFeature with AsyncBeforeAfterActionFeature with RequestScopeFeature with LoggerProvider =>
 
   /**
    * Overrides Scalatra's default key name.
@@ -89,7 +89,7 @@ trait CSRFProtectionFeature extends CSRFTokenSupport {
   def handleForgeryIfDetected(): Unit = halt(403)
 
   // Registers csrfKey & csrfToken to request scope.
-  beforeAction() {
+  beforeAction() { implicit ctx =>
     if (getFromRequestScope(RequestScopeFeature.ATTR_CSRF_KEY)(context).isEmpty) {
       set(RequestScopeFeature.ATTR_CSRF_KEY, csrfKey)(context)
       set(RequestScopeFeature.ATTR_CSRF_TOKEN, prepareCsrfToken())(context)

framework/src/main/scala/skinny/controller/feature/AsyncXFrameOptionsHeaderFeature.scala
@@ -7,7 +7,7 @@ import skinny.micro.SkinnyMicroBase
  *
  * - https://www.owasp.org/index.php/List_of_useful_HTTP_headers
  */
-trait XFrameOptionsHeaderFeature { self: SkinnyMicroBase with BeforeAfterActionFeature =>
+trait AsyncXFrameOptionsHeaderFeature { self: SkinnyMicroBase with AsyncBeforeAfterActionFeature =>
 
   // NOTE: There are three possible values for the X-Frame-Options headers.
   // - "deny" which prevents any domain from framing the content.
@@ -16,8 +16,8 @@ trait XFrameOptionsHeaderFeature { self: SkinnyMicroBase with BeforeAfterActionF
   lazy val xFrameOptionsPolicy = "sameorigin"
 
   // NOTE: for all HTML responses defined as Skinny routes
-  beforeAction() {
-    response(context).setHeader("X-Frame-Options", xFrameOptionsPolicy)
+  beforeAction() { implicit ctx =>
+    response.setHeader("X-Frame-Options", xFrameOptionsPolicy)
   }
 
 }

framework/src/main/scala/skinny/controller/feature/AsyncXXSSProtectionHeaderFeature.scala
@@ -7,10 +7,10 @@ import skinny.micro.SkinnyMicroBase
  *
  * - https://www.owasp.org/index.php/List_of_useful_HTTP_headers
  */
-trait XXSSProtectionHeaderFeature { self: SkinnyMicroBase with BeforeAfterActionFeature =>
+trait AsyncXXSSProtectionHeaderFeature { self: SkinnyMicroBase with AsyncBeforeAfterActionFeature =>
 
   // NOTE: for all HTML responses defined as Skinny routes
-  beforeAction() {
+  beforeAction() { implicit ctx =>
     response(context).setHeader("X-XSS-Protection", "1; mode=block")
   }
 

framework/src/main/scala/skinny/controller/feature/XContentTypeOptionsNosniffHeaderFeature.scala
@@ -14,6 +14,7 @@ import skinny.micro.base.BeforeAfterDsl
 trait XContentTypeOptionsNosniffHeaderFeature { self: SkinnyMicroBase with BeforeAfterDsl =>
 
   // NOTE: To force this header to all responses
+  // beforeAction() {
   before() {
     response(context).setHeader("X-Content-Type-Options", "nosniff")
   }