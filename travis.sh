07e43b016d3c260b2d493d789062958a821b13e3
Kazuhiro Sera <seratch@gmail.com>
2015-11-04 14:20:52 +0900

    Fix Travis build stability

--- travis.sh
+++ travis.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+if [[ "$TRAVIS_SCALA_VERSION" == "" ]]; then
+  TRAVIS_SCALA_VERSION=2.11.7
+fi
+if [[ "$TEST_TYPE" == "" ]]; then
+  TEST_TYPE=framework
+fi
+
 if [[ "$TEST_TYPE" == "framework" ]]; then
   export SKINNY_ENV=test
   export APP_ENV=test
   gem install sass &&
-  sbt clean ++$TRAVIS_SCALA_VERSION "example/run db:migrate test" test
+  sbt "example/run db:migrate test" &&
+  sbt ++$TRAVIS_SCALA_VERSION test
 elif [[ "$TEST_TYPE" == "blank-app" && "$TRAVIS_SCALA_VERSION" == 2.11* ]]; then
   export SBT_OPTS="" &&  yes|./run_skinny-blank-app_test.sh
 fi
