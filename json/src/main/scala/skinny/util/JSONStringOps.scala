206aac6f6099d30fb9718aebaa9057e112a4d592
Kazuhiro Sera <seratch@gmail.com>
2015-11-22 15:30:42 +0900

    Add deprecation for 1.x users

---
+++ json/src/main/scala/skinny/util/JSONStringOps.scala
@@ -0,0 +1,7 @@
+package skinny.util
+
+@deprecated("Use skinny.json4s.JSONStringOps instead", since = "2.0.0")
+trait JSONStringOps extends skinny.json4s.JSONStringOps
+
+@deprecated("Use skinny.json4s.JSONStringOps instead", since = "2.0.0")
+object JSONStringOps extends JSONStringOps
