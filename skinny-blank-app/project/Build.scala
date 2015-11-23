019a273d1c87dcc70f9d49c85679b2dee0cb0e1f
Kazuhiro Sera <seratch@gmail.com>
2015-11-23 11:51:33 +0900

    Fix version for blank project

--- skinny-blank-app/project/Build.scala
+++ skinny-blank-app/project/Build.scala
@@ -15,7 +15,7 @@ object SkinnyAppBuild extends Build {
   val appName = "skinny-blank-app"
   val appVersion = "0.1.0-SNAPSHOT"
 
-  val skinnyVersion = "2.0.0"
+  val skinnyVersion = "2.0.1"
   val theScalaVersion = "2.11.7"
   val jettyVersion = "9.2.14.v20151106"
 
