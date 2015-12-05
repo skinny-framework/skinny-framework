d5c2186c8dc882fc010db84193ff58bf96d67a99
Kazuhiro Sera <seratch@gmail.com>
2015-12-05 11:09:41 +0900

    version 2.0.2

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import scala.language.postfixOps
 
 object SkinnyFrameworkBuild extends Build {
 
-  lazy val currentVersion = "2.0.1"
+  lazy val currentVersion = "2.0.2"
 
   lazy val skinnyMicroVersion = "1.0.0"
   // Scalatra 2.4 will be incompatible with Skinny
