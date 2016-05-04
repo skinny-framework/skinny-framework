484e3f9be233a660f4ff4e8a451f1f2d21b975c3
Kaz Sera <seratch@gmail.com>
2016-05-04 14:02:58 +0900

    version 2.0.9
     - [framework] Bump slf4j and others' minor version by @seratch
     - [test] #316 Fixed NullPointerException in MockController with SkinnySessionFilter by @BlackPrincess
     - [http-client] #313 skinny-http-client can not parse `Set-Cookie` what contains path atribute #312 by @gakuzzzz

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import scala.language.postfixOps
 
 object SkinnyFrameworkBuild extends Build {
 
-  lazy val currentVersion = "2.0.9-SNAPSHOT"
+  lazy val currentVersion = "2.0.9"
 
   lazy val skinnyMicroVersion = "1.0.4"
   lazy val scalatraTestVersion = "2.4.0"
