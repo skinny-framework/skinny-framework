57f505becef007f8ef0ecf55efb9ff9a3741f80b
Kaz Sera <seratch@gmail.com>
2016-05-08 00:12:17 +0900

    Fix Travis settings

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_SCALA_VERSION" == "" ]]; then
-  TRAVIS_SCALA_VERSION=2.11.7
+  TRAVIS_SCALA_VERSION=2.11.8
 fi
 if [[ "$TEST_TYPE" == "" ]]; then
   TEST_TYPE=framework
