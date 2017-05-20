0da9c6af0f0c378bf430210d28b5c77116ecd6ea
Kazuhiro Sera <seratch@gmail.com>
2017-05-20 18:56:49 +0900

    Improve the workaround

--- orm/src/test/scala/skinny/DBSettingsSpec.scala
+++ orm/src/test/scala/skinny/DBSettingsSpec.scala
@@ -4,14 +4,18 @@ import org.scalatest.FlatSpec
 
 class DBSettingsSpec extends FlatSpec with DBSettings with DBSettingsInitializer {
 
-  System.setProperty(SkinnyEnv.EnvKey, "test")
-
   behavior of "DBSettings"
 
   it should "have #initialize" in {
-    initialize(false)
-    initialize(true)
-    destroy()
+    val originalValue = System.getProperty(SkinnyEnv.EnvKey)
+    try {
+      System.setProperty(SkinnyEnv.EnvKey, "test")
+      initialize(false)
+      initialize(true)
+      destroy()
+    } finally {
+      System.setProperty(SkinnyEnv.EnvKey, originalValue)
+    }
   }
 
 }
