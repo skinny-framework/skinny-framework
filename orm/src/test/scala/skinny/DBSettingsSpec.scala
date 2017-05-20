65fdf7307ea8c8a25804566cb5c4b129fc9a7298
Kazuhiro Sera <seratch@gmail.com>
2017-05-20 18:36:18 +0900

    Add a workaround to fix a test so far

--- orm/src/test/scala/skinny/DBSettingsSpec.scala
+++ orm/src/test/scala/skinny/DBSettingsSpec.scala
@@ -4,6 +4,8 @@ import org.scalatest.FlatSpec
 
 class DBSettingsSpec extends FlatSpec with DBSettings with DBSettingsInitializer {
 
+  System.setProperty(SkinnyEnv.EnvKey, "test")
+
   behavior of "DBSettings"
 
   it should "have #initialize" in {
