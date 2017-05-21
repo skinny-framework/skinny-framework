b58cba949d6822bc5a5d6b492948557ea54c43ed
Kazuhiro Sera <seratch@gmail.com>
2017-05-21 10:15:03 +0900

    Drop Scala 2.10 support

--- publish_local.sh
+++ publish_local.sh
@@ -63,28 +63,6 @@ sbt ++2.12.2 \
   scaldi/publishLocal \
   freemarker/publishLocal \
   thymeleaf/publishLocal \
-  velocity/publishLocal \
-  ++2.10.6 \
-  clean \
-  common/publishLocal \
-  assets/publishLocal \
-  httpClient/publishLocal \
-  json/publishLocal \
-  orm/publishLocal \
-  factoryGirl/publishLocal \
-  validator/publishLocal \
-  framework/publishLocal \
-  mailer/publishLocal \
-  task/publishLocal \
-  test/publishLocal \
-  worker/publishLocal \
-  oauth2/publishLocal \
-  oauth2Controller/publishLocal \
-  twitterController/publishLocal \
-  standalone/publishLocal \
-  scaldi/publishLocal \
-  freemarker/publishLocal \
-  thymeleaf/publishLocal \
   velocity/publishLocal
 
 fi
