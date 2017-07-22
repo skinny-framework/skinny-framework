980667ecc451af9a7399700cdcfc16602e5b0dfa
Kazuhiro Sera <seratch@gmail.com>
2017-07-22 21:07:26 +0900

    Fix sass installation error on Travis env

--- travis.sh
+++ travis.sh
@@ -10,7 +10,8 @@ fi
 if [[ "$TEST_TYPE" == "framework" ]]; then
   export SKINNY_ENV=test
   export APP_ENV=test
-  gem install sass &&
+  # sass 3.5 requires Ruby 2.0+
+  gem install sass -v 3.4.25 &&
   sbt "example/run db:migrate test" &&
   sbt ++$TRAVIS_SCALA_VERSION test
 elif [[ "$TEST_TYPE" == "blank-app" && "$TRAVIS_SCALA_VERSION" == 2.12* ]]; then
