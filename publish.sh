4890b1dd1bef43e60dec9368055e055069a9bcc0
Kazuhiro Sera <seratch@gmail.com>
2017-07-23 17:04:59 +0900

    Revert "Drop Scala 2.10 support"
    This reverts commit b58cba949d6822bc5a5d6b492948557ea54c43ed.

--- publish.sh
+++ publish.sh
@@ -43,4 +43,26 @@ sbt ++2.12.2 \
   scaldi/publishSigned \
   freemarker/publishSigned \
   thymeleaf/publishSigned \
+  velocity/publishSigned \
+  ++2.10.6 \
+  clean \
+  common/publishSigned \
+  assets/publishSigned \
+  httpClient/publishSigned \
+  json/publishSigned \
+  orm/publishSigned \
+  factoryGirl/publishSigned \
+  validator/publishSigned \
+  framework/publishSigned \
+  mailer/publishSigned \
+  task/publishSigned \
+  test/publishSigned \
+  worker/publishSigned \
+  oauth2/publishSigned \
+  oauth2Controller/publishSigned \
+  twitterController/publishSigned \
+  standalone/publishSigned \
+  scaldi/publishSigned \
+  freemarker/publishSigned \
+  thymeleaf/publishSigned \
   velocity/publishSigned
