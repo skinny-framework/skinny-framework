3e87ec2e6e2828f624ad07349906c87cdf70b411
Kazuhiro Sera <seratch@gmail.com>
2018-04-01 14:12:29 +0900

    Fix release scripts

--- publish.sh
+++ publish.sh
@@ -43,26 +43,4 @@ sbt ++2.12.4 \
   scaldi/publishSigned \
   freemarker/publishSigned \
   thymeleaf/publishSigned \
-  velocity/publishSigned \
-  ++2.10.7 \
-  clean \
-  common/publishSigned \
-  assets/publishSigned \
-  httpClient/publishSigned \
-  json/publishSigned \
-  orm/publishSigned \
-  factoryGirl/publishSigned \
-  validator/publishSigned \
-  framework/publishSigned \
-  mailer/publishSigned \
-  task/publishSigned \
-  test/publishSigned \
-  worker/publishSigned \
-  oauth2/publishSigned \
-  oauth2Controller/publishSigned \
-  twitterController/publishSigned \
-  standalone/publishSigned \
-  scaldi/publishSigned \
-  freemarker/publishSigned \
-  thymeleaf/publishSigned \
   velocity/publishSigned
