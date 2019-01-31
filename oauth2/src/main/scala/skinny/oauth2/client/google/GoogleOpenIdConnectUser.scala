9d3f2c114c952af857f32f0b02d3f3703de051be
Motohiro OHNO <m-ohno@atware.co.jp>
2019-01-31 22:08:51 +0900

    Execute scalafmt

--- oauth2/src/main/scala/skinny/oauth2/client/google/GoogleOpenIdConnectUser.scala
+++ oauth2/src/main/scala/skinny/oauth2/client/google/GoogleOpenIdConnectUser.scala
@@ -6,14 +6,14 @@ import skinny.oauth2.client.OAuth2User
   * Authorized Google OpenID Connect user
   */
 case class GoogleOpenIdConnectUser(
-  sub: String,
-  email: String,
-  emailVerified: Boolean,
-  name: String,
-  familyName: String,
-  givenName: String,
-  locale: String,
-  picture: String,
+    sub: String,
+    email: String,
+    emailVerified: Boolean,
+    name: String,
+    familyName: String,
+    givenName: String,
+    locale: String,
+    picture: String,
 ) extends OAuth2User {
   override val id: String = sub
 }
